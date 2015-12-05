cf4ac1f0ee0aa3c95429f705b19e6c494551b524
xushu42 <xushu@gmx.net>
2015-12-05 13:58:47 +0100

    initial templates

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
+
 var app = express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('home');
+});
+
+app.get('/users/emmy', function (req, res) {
+  res.render('dashboard');
+});
+
+app.get('/users/emmy/settings', function (req, res) {
+  res.render('settings');
 });
 
 var server = app.listen(3000, function () {
